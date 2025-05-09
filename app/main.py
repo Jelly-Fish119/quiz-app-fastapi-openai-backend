43fda71bb75384ae3ce5fdb6547bfb6ad6046039
Jelly-Fish119 <skmoinuddin0800@gmail.com>
2025-05-09 05:02:53 +0300

    allow all domain

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.routes import router
+
+app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+app.include_router(router, prefix="/api") 
