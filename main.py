9fcf54fe57520924807f6071f825f07413339f73
Jelly-Fish119 <skmoinuddin0800@gmail.com>
2025-05-15 17:02:48 +0300

    add logger of topics and chapters

--- main.py
+++ main.py
@@ -456,6 +456,8 @@ Remember:
         
         # Add topic and chapter information to each question
         if topics and chapters:
+            print("chapters: " chapters)
+            print("topics: ", topics)
             for question in questions:
                 question['topic'] = find_best_matching_topic(question['question'], topics)
                 question['chapter'] = find_best_matching_chapter(question['question'], chapters)
